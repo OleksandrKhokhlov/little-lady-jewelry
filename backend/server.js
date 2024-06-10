e6066c59314050874d9a64bf55e2a38fbdfa2afc
OleksandrKhokhlov <hohlovsanya@gmail.com>
2024-06-10 18:57:52 +0300

    Setting backend project

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = require("./app");
+
+const DB_HOST = process.env.DB_HOST;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
