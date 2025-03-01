985de826fa211852e8fa83696b064a0c5d36e68c
OleksandrKhokhlov <hohlovsanya@gmail.com>
2025-03-01 18:55:37 +0200

    refactoring header & bag fix

--- frontend/little-lady-jewelry/src/app/components/shared/header.tsx
+++ frontend/little-lady-jewelry/src/app/components/shared/header.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useState } from "react";
-import { usePathname } from "next/navigation";
 import { Container } from "./container";
 import Icon from "../ui/icon";
 import Link from "next/link";
 import Button from "../ui/button";
-import Modal from "../ui/modal";
+import { Logo } from "../ui/logo";
+import { ModalBurgerMenu } from "./modalBurgerMenu";
 
 interface Props {
   className?: string;
@@ -14,7 +14,6 @@ interface Props {
 export const Header: React.FC<Props> = ({ className }) => {
   const [isModalBurgerOpen, setModalBurgerOpen] = useState(false);
   const [isModalCartOpen, setModalCartOpen] = useState(false);
-  const pathname = usePathname();
 
   return (
     <header className={className}>
@@ -27,12 +26,7 @@ export const Header: React.FC<Props> = ({ className }) => {
           }}
           className="md:hidden  w-[72px]"
         />
-        <Link href={"/"} className="text-center">
-          <h1 className="font-kallithea">Little lady</h1>
-          <h2 className="font-calisto text-[10px] text-[var(--accent-color)] uppercase">
-            Jewelry
-          </h2>
-        </Link>
+        <Logo />
         <div className="flex items-center justify-between gap-11">
           <Link href={"./favorite"}>
             <Icon iconId="icon-Heart" className="size-[15px]" />
@@ -45,44 +39,10 @@ export const Header: React.FC<Props> = ({ className }) => {
             }}
           />
         </div>
-        <Modal
-          isOpen={isModalBurgerOpen}
-          onClose={() => setModalBurgerOpen(false)}
-          side="left"
-        >
-          <nav>
-            <ul className="flex flex-col gap-4 mt-2 ">
-              <li>
-                <Link
-                  href={"/"}
-                  onClick={() => setModalBurgerOpen(false)}
-                  className={`px-1 border-b-2 border-[var(--text-color)] w-fit  ${pathname === "/" ? "text-[var(--hover-color)] border-[var(--hover-color)]" : ""}`}
-                >
-                  Головна
-                </Link>
-              </li>
-              <li>
-                {" "}
-                <Link
-                  href={"./favorite"}
-                  onClick={() => setModalBurgerOpen(false)}
-                  className={`px-1 border-b-2 border-[var(--text-color)] w-fit ${pathname === "/favorite" ? "text-[var(--hover-color)] border-[var(--hover-color)]" : ""}`}
-                >
-                  Улюблене
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"./deliveryAndPayment"}
-                  onClick={() => setModalBurgerOpen(false)}
-                  className={`px-1 border-b-2 border-[var(--text-color)] w-fit ${pathname === "/deliveryAndPayment" ? "text-[var(--hover-color)] border-[var(--hover-color)]" : ""}`}
-                >
-                  Доставка та оплата
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </Modal>
+        <ModalBurgerMenu
+          isModalBurgerOpen={isModalBurgerOpen}
+          setModalBurgerOpen={() => setModalBurgerOpen(false)}
+        />
       </Container>
     </header>
   );
--- frontend/little-lady-jewelry/src/app/components/ui/modal.tsx
+++ frontend/little-lady-jewelry/src/app/components/ui/modal.tsx
@@ -5,25 +5,39 @@ interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   side?: "left" | "right";
+  className: string;
   children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, side = "right", children }) => {
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  side = "right",
+  className,
+  children,
+}) => {
   return (
     <div
       className={`fixed inset-0 bg-black bg-opacity-50 transition-opacity ${
         isOpen ? "opacity-100 visible" : "opacity-0 invisible"
-      }`}
+      } ${className}`}
       onClick={onClose}
     >
       <div
-        className={`fixed top-0 ${side === "right" ? "right-0" : "left-0"} h-full w-60 bg-[var(--bg-color)] shadow-lg p-6 transition-transform duration-300 ${
-          isOpen ? "translate-x-0" : side === "right" ? "translate-x-full" : "-translate-x-full"
+        className={`fixed top-0 ${side === "right" ? "right-0 rounded-l-xl" : "left-0 rounded-r-xl"} h-full w-60 bg-[var(--bg-color)] shadow-lg p-6 transition-transform duration-300 ${
+          isOpen
+            ? "translate-x-0"
+            : side === "right"
+              ? "translate-x-full"
+              : "-translate-x-full"
         }`}
         onClick={(e) => e.stopPropagation()}
       >
-        <button onClick={onClose} className="absolute top-2 right-2 hover:text-black">
-          <Icon iconId="icon-Cross" className="size-[15px]"/>
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 hover:text-black"
+        >
+          <Icon iconId="icon-Cross" className="size-[15px]" />
         </button>
         {children}
       </div>
