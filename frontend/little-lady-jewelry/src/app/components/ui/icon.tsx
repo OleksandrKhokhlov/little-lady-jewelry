fa80f72de99fe65533f6fcfb462ffcbb424d9eed
OleksandrKhokhlov <hohlovsanya@gmail.com>
2025-06-09 21:36:07 +0300

    bag fix icon

--- frontend/little-lady-jewelry/src/app/components/ui/icon.tsx
+++ frontend/little-lady-jewelry/src/app/components/ui/icon.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 interface IconProps {
-  iconId: string; 
-  className?: string; 
+  iconId: string;
+  className?: string;
+  color?: string;
 }
 
-const Icon: React.FC<IconProps> = ({ iconId, className }) => (
+const Icon: React.FC<IconProps> = ({
+  iconId,
+  className,
+}) => (
   <svg className={className}>
-    <use href={`/icons.svg#${iconId}`} />
+    <use href={`/icons.svg#${iconId}`}/>
   </svg>
 );
 
