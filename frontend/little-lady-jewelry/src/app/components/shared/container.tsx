68d118887edd785566856c4eed0418a0d4fa2539
OleksandrKhokhlov <hohlovsanya@gmail.com>
2025-06-14 20:59:39 +0300

    mod container

--- frontend/little-lady-jewelry/src/app/components/shared/container.tsx
+++ frontend/little-lady-jewelry/src/app/components/shared/container.tsx
@@ -3,8 +3,11 @@ import { cn } from "@/lib/cn";
 
 interface Props {
     className?: string;
+    tag?: keyof JSX.IntrinsicElements;
 }
 
-export const Container: React.FC<React.PropsWithChildren<Props>> = ({ className, children }) => {
-    return <div className={cn('mx-auto max-w-[400px] px-[10px] pt-[5px]', className)}>{ children}</div>
+export const Container: React.FC<React.PropsWithChildren<Props>> = ({ className, children, tag = 'div' }) => {
+    const Tag = tag;
+
+    return <Tag className={cn('mx-auto max-w-[400px] px-[10px] pt-[5px]', className)}>{ children}</Tag>
 }
