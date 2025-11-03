93a7101910065b98cb04110250ed6ccaf66ebfd4
OleksandrKhokhlov <hohlovsanya@gmail.com>
2025-11-03 14:38:56 +0200

    feat: add create product

--- frontend/little-lady-jewelry/src/components/adminProductEditor.tsx
+++ frontend/little-lady-jewelry/src/components/adminProductEditor.tsx
@@ -1,33 +1,35 @@
 "use client";
 
-import { updateProdukt } from "@/app/api";
+import { addProdukt, updateProdukt } from "@/app/api";
 import { Field, Form, Formik } from "formik";
 import { useState } from "react";
 import { ImageUploader } from "./imageUploader";
 import toast from "react-hot-toast";
 import { Button } from "./button";
 
-interface AdminProductEditorProps {
-  product: {
-    _id: string;
-    name: string;
-    images: Array<{ public_id: string; url: string }>;
-    video?: string;
-    price: number;
-    type:
-      | "пусети на заглушках"
-      | "пусети на закрутках"
-      | "англійський замок"
-      | "конго";
-    material: string;
-    insert: string;
-    weight: number;
-    dimensions: {
-      height?: number;
-      width?: number;
-    };
-    quantity: number;
+interface ProductDetails {
+  _id: string;
+  name: string;
+  images: Array<{ public_id: string; url: string }>;
+  video?: string;
+  price: number;
+  type:
+    | "пусети на заглушках"
+    | "пусети на закрутках"
+    | "англійський замок"
+    | "конго";
+  material: string;
+  insert: string;
+  weight: number;
+  dimensions: {
+    height?: number;
+    width?: number;
   };
+  quantity: number;
+}
+
+interface AdminProductEditorProps {
+  product?: ProductDetails;
 }
 
 interface FormValues {
@@ -48,65 +50,92 @@ interface FormValues {
   images: string[];
 }
 
+const DEFAULT_PRODUCT_VALUES = {
+  name: "",
+  price: 0,
+  quantity: 0,
+  video: "",
+  type: "пусети на заглушках" as const,
+  material: "",
+  insert: "",
+  weight: 0,
+  width: 0,
+  height: 0,
+  images: [] as string[],
+};
+
 export const AdminProductEditor: React.FC<AdminProductEditorProps> = ({
   product,
 }) => {
-  const [initialImages, setInitialImages] = useState<string[]>(
-    product.images?.map((img) => img.url),
-  );
-  const {
-    name,
-    price,
-    quantity,
-    video,
-    type,
-    material,
-    insert,
-    weight,
-    dimensions = {},
-  } = product;
+  const isEditMode = !!product?._id;
+  const initialImagesUrls = product?.images?.map((img) => img.url) || [];
+  const [initialImages] = useState<string[]>(initialImagesUrls);
 
   const initialValues: FormValues = {
-    name,
-    price,
-    quantity,
-    video: video || "",
-    type,
-    material,
-    insert,
-    weight,
-    width: dimensions.width || 0,
-    height: dimensions.height || 0,
+    name: product?.name || DEFAULT_PRODUCT_VALUES.name,
+    price: product?.price || DEFAULT_PRODUCT_VALUES.price,
+    quantity: product?.quantity || DEFAULT_PRODUCT_VALUES.quantity,
+    video: product?.video || DEFAULT_PRODUCT_VALUES.video,
+    type: product?.type || DEFAULT_PRODUCT_VALUES.type,
+    material: product?.material || DEFAULT_PRODUCT_VALUES.material,
+    insert: product?.insert || DEFAULT_PRODUCT_VALUES.insert,
+    weight: product?.weight || DEFAULT_PRODUCT_VALUES.weight,
+    width: product?.dimensions?.width || DEFAULT_PRODUCT_VALUES.width,
+    height: product?.dimensions?.height || DEFAULT_PRODUCT_VALUES.height,
     images: initialImages,
+  };
+
+  const formTitle = isEditMode
+    ? `Редагування продукту: ${product?.name}`
+    : "Створення нового продукту";
+
+  const buttonText = isEditMode ? "Зберегти зміни" : "Створити продукт";
+
+  const handleSubmit = async (
+    values: FormValues,
+    { setSubmitting, resetForm }: any,
+  ) => {
+    try {
+      const { width, height, images, ...rest } = values;
+      const payload: any = {
+        ...rest,
+        dimensions: {
+          width,
+          height,
+        },
+        images,
+      };
+
+      if (isEditMode && product?._id) {
+        const payloadToUpdate: any = { ...payload };
+        if (JSON.stringify(initialImagesUrls) === JSON.stringify(images)) {
+          delete payloadToUpdate.images;
+        }
+        await updateProdukt(product._id, payloadToUpdate);
+        toast.success("Продукт успішно оновлено!");
+      } else {
+        await addProdukt(payload);
+        toast.success("Продукт успішно створено!");
+        resetForm({ values: DEFAULT_PRODUCT_VALUES });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(
+        isEditMode
+          ? "Сталася помилка під час оновлення продукту."
+          : "Сталася помилка під час створення продукту.",
+      );
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
     <div className="max-w-lg mx-auto bg-white p-2 rounded-lg shadow-md border border-gray-100">
+      <h2 className="text-2xl font-bold text-center">{formTitle}</h2>
       <Formik<FormValues>
         initialValues={initialValues}
-        onSubmit={async (values, { setSubmitting }) => {
-          try {
-            const { width, height, images, ...rest } = values;
-            const payload: any = {
-              ...rest,
-              dimensions: {
-                width,
-                height,
-              },
-            };
-
-            if (JSON.stringify(initialImages) !== JSON.stringify(images)) {
-              payload.images = images;
-            }
-            await updateProdukt(product._id, payload);
-            toast.success("Продукт успішно оновлено!");
-          } catch (error) {
-            console.error(error);
-            toast.error("Сталася помилка під час оновлення продукту.");
-          } finally {
-            setSubmitting(false);
-          }
-        }}
+        onSubmit={handleSubmit}
       >
         {({ values, setFieldValue, isSubmitting }) => (
           <Form className="flex flex-col gap-1">
@@ -169,7 +198,7 @@ export const AdminProductEditor: React.FC<AdminProductEditorProps> = ({
             </label>
             <Button
               type="submit"
-              text={isSubmitting ? "Збереження..." : "Зберегти зміни"}
+              text={isSubmitting ? "Збереження..." : buttonText}
               disabled={isSubmitting}
               className="w-[150px] h-[30px] mx-auto bg-[var(--accent-color)] text-white font-[400] rounded-md text-[12px] p-1"
             />
