e1da04f0a246dc0267c0fc26000ef207a2d7eaba
OleksandrKhokhlov <hohlovsanya@gmail.com>
2025-11-04 19:08:28 +0200

    feat: add counter in favorite & cart

--- frontend/little-lady-jewelry/src/components/header.tsx
+++ frontend/little-lady-jewelry/src/components/header.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { Container } from "./container";
 import { Icon } from "./icon";
@@ -7,14 +8,21 @@ import { Button } from "./button";
 import { Logo } from "./logo";
 import { ModalBurgerMenu } from "./modalBurgerMenu";
 import { ModalCart } from "./modalCart";
+import { useProduktContext } from "@/lib";
 
 interface Props {
   className?: string;
 }
 
 export const Header: React.FC<Props> = ({ className }) => {
+  const { favoriteProdukts, inCart } = useProduktContext();
   const [isModalBurgerOpen, setModalBurgerOpen] = useState(false);
   const [isModalCartOpen, setModalCartOpen] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <header className={className}>
@@ -34,24 +42,36 @@ export const Header: React.FC<Props> = ({ className }) => {
         />
         <Logo />
         <div className="flex items-center justify-between gap-11">
-          <Link href={"./favorite"}>
+          <Link href={"./favorite"} className="relative">
             <Icon
               iconId="icon-Heart"
-              className="size-[15px] fill-[var(--accent-color)]"
+              className="size-[15px] fill-[var(--accent-color)] stroke-2"
             />
+            {favoriteProdukts.length > 0 && isClient && (
+              <span className="absolute top-0 left-[50%] transform -translate-x-1/2 text-[10px] block">
+                {favoriteProdukts.length}
+              </span>
+            )}
           </Link>
           <Button
+            className="relative"
             icon={
               <Icon
                 iconId="icon-Cart"
-                className="w-[13px] h-[15px] fill-[var(--accent-color)]"
+                className=" w-[13px] h-[15px] fill-[var(--accent-color)]"
               />
             }
             onClick={() => {
               setModalCartOpen(true);
               setModalBurgerOpen(false);
             }}
-          />
+          >
+            {isClient && inCart.length > 0 && (
+              <span className="absolute top-[55%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-[10px] block">
+                {inCart.length}
+              </span>
+            )}
+          </Button>
         </div>
         <ModalBurgerMenu
           isModalBurgerOpen={isModalBurgerOpen}
