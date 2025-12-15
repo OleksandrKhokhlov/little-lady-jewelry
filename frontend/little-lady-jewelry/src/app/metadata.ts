e3b113efe1e93de7802586b08955af1893fb660f
OleksandrKhokhlov <hohlovsanya@gmail.com>
2025-12-15 11:25:15 +0200

    fix: correct metadata

--- frontend/little-lady-jewelry/src/app/metadata.ts
+++ frontend/little-lady-jewelry/src/app/metadata.ts
@@ -14,12 +14,12 @@ const metadata: Metadata = {
       url: "/favicon_io/apple-touch-icon.png",
     },
     {
-      rel: "android-chrome-icon",
+      rel: "icon",
       sizes: "192x192",
       url: "/favicon_io/android-chrome-192x192.png",
     },
     {
-      rel: "android-chrome-icon",
+      rel: "icon",
       sizes: "512x512",
       url: "/favicon_io/android-chrome-512x512.png",
     },
