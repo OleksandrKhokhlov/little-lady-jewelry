9b4f21490e4f6626fa6c1a52d83070b9d499f2d4
OleksandrKhokhlov <hohlovsanya@gmail.com>
2026-01-05 13:50:57 +0200

    feat: update site.webmanifest paths and add link in layout for better asset management

--- frontend/little-lady-jewelry/src/app/layout.tsx
+++ frontend/little-lady-jewelry/src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="site.webmanifest" href="/favicon_io/site.webmanifest" />
       </head>
       <body className="font-cormorant">
         <ProductProvider>
