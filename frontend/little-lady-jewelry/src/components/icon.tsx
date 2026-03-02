725ebec61cadc490671fb49270e9280f0b8f1a97
OleksandrKhokhlov <hohlovsanya@gmail.com>
2026-03-02 20:22:05 +0200

    ref: remove unnecessary imports

--- frontend/little-lady-jewelry/src/components/icon.tsx
+++ frontend/little-lady-jewelry/src/components/icon.tsx
@@ -1,13 +1,11 @@
 import { cn } from "@/lib";
-import React from "react";
 
 interface IconProps {
   iconId: string;
   className?: string;
-  color?: string;
 }
 
-export const Icon: React.FC<IconProps> = ({ iconId, className }) => (
+export const Icon = ({ iconId, className }: IconProps) => (
   <svg className={cn("size-[100%]", className)}>
     <use href={`/icons.svg#${iconId}`} />
   </svg>
