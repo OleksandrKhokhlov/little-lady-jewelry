fbf00b5051ef21e3fb72690c2dabd3d7a201b4d5
OleksandrKhokhlov <hohlovsanya@gmail.com>
2026-04-13 19:46:44 +0300

    feat: add f compressingImeges

--- frontend/little-lady-jewelry/src/lib/index.ts
+++ frontend/little-lady-jewelry/src/lib/index.ts
@@ -3,3 +3,4 @@ export { ProductProvider, useProduktContext } from "./productContext";
 export { getLocalStorage } from "./localStorage";
 export { parseTime } from "./parseTime";
 export { getInitialLimit } from "./getInitialLimit";
+export { compressImg } from "./compressImg";
