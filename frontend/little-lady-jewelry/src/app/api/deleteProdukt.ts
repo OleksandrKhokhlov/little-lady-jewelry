86497c649fde0a776d9a60740ccc6bc4cfa4cef5
OleksandrKhokhlov <hohlovsanya@gmail.com>
2026-04-29 18:42:37 +0300

    fix(api): remove toast notifications from deleteProdukt function

--- frontend/little-lady-jewelry/src/app/api/deleteProdukt.ts
+++ frontend/little-lady-jewelry/src/app/api/deleteProdukt.ts
@@ -1,17 +1,13 @@
-import toast from "react-hot-toast";
 import { api } from "./api";
 
 export const deleteProdukt = async (productId: string) => {
   try {
     const res = await api.delete(`/product/${productId}`);
     if (res.status !== 200) {
-      toast.error("Щось пішло не так, спробуйте ще раз");
       throw new Error("Failed to delete product");
     }
-    toast.success("Товар успішно видалено");
     return res.data;
   } catch (error) {
-    toast.error("Щось пішло не так, спробуйте ще раз");
     console.log(error);
     return null;
   }
